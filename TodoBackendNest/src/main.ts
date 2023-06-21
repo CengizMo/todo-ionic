27db8199a8dbfcaa9648d47aa0e7bd6b2664a5dc
Cengiz <cengiz.monteforte@plan-b-gmbh.com>
2023-06-21 12:05:14 +0200

    added swagger to backend

--- TodoBackendNest/src/main.ts
+++ TodoBackendNest/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('ToDo App')
+  .setDescription('This is for a To-Do app built with Ionic (Angular) and NestJS')
+  .setVersion('v1')
+  .addTag('todos')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(3000);
 }
